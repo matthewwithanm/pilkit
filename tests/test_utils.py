e0e0fcbe5e24df5d66fae94c14b9938fb3493d3d
Matthew Tretter <m@tthewwithanm.com>
2013-02-12 22:17:45 -0500

    Add default extensions test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -21,3 +21,16 @@ def test_unknown_format():
 @raises(UnknownExtension)
 def test_unknown_extension():
     extension_to_format('.txt')
+
+
+def test_default_extension():
+    """
+    Ensure default extensions are honored.
+
+    Since PIL's ``Image.EXTENSION`` lists ``'.jpe'`` before the more common
+    JPEG extensions, it would normally be the extension we'd get for that
+    format. ``pilkit.utils.DEFAULT_EXTENSIONS`` is our way of specifying which
+    extensions we'd prefer, and this tests to make sure it's working.
+
+    """
+    eq_(format_to_extension('JPEG'), '.jpg')
