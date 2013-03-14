978c94a5ace892c6d1c7317384d424f42c14d3c7
Matthew Tretter <m@tthewwithanm.com>
2013-03-13 23:30:41 -0400

    Test that Resize processor antialiases
    Related: jdriscoll/django-imagekit#192

--- tests/test_processors.py
+++ tests/test_processors.py
@@ -1,6 +1,6 @@
-from pilkit.lib import Image
-from pilkit.processors import ResizeToFill, ResizeToFit, SmartCrop
-from nose.tools import eq_
+from pilkit.lib import Image, ImageDraw
+from pilkit.processors import Resize, ResizeToFill, ResizeToFit, SmartCrop
+from nose.tools import eq_, assert_true
 from .utils import create_image
 
 
@@ -29,3 +29,33 @@ def test_resizetofit_mat():
     img = Image.new('RGB', (200, 100))
     img = ResizeToFit(100, 100, mat_color=0x000000).process(img)
     eq_(img.size, (100, 100))
+
+
+def test_resize_antialiasing():
+    """
+    Test that the Resize processor antialiases.
+
+    The Resize processor is used by all of the Resize* variants, so this should
+    cover all of resize processors. Basically, this is to test that it converts
+    to RGBA mode before resizing.
+
+    Related: jdriscoll/django-imagekit#192
+
+    """
+    # Create a palette image and draw a circle into it.
+    img = Image.new('P', (500, 500), 1)
+    img.putpalette([
+        0,   0,   0,
+        255, 255, 255,
+        0,   0,   255,
+    ])
+    d = ImageDraw.ImageDraw(img)
+    d.ellipse((100, 100, 400, 400), fill=2)
+
+    # Resize the image using the Resize processor
+    img = Resize(100, 100).process(img)
+
+    # Count the number of colors
+    color_count = len(filter(None, img.histogram()))
+
+    assert_true(color_count > 2)
