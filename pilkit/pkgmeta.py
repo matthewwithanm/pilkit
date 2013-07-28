5ee0507e48ec1d5de4aa3f69b4982800c66bda8b
Matthew Tretter <m@tthewwithanm.com>
2013-07-27 21:34:40 -0400

    Bump version to 1.1.0

--- pilkit/pkgmeta.py
+++ pilkit/pkgmeta.py
@@ -1,5 +1,5 @@
 __title__ = 'pilkit'
 __author__ = 'Matthew Tretter'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __license__ = 'BSD'
 __all__ = ['__title__', '__author__', '__version__', '__license__']
