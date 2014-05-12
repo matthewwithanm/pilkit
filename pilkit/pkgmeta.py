e00cd58ac7aebb3c88d3af65628e82d68ef29550
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-05-12 13:51:37 -0400

    Bump version to 1.1.8

--- pilkit/pkgmeta.py
+++ pilkit/pkgmeta.py
@@ -1,5 +1,5 @@
 __title__ = 'pilkit'
 __author__ = 'Matthew Tretter'
-__version__ = '1.1.7'
+__version__ = '1.1.8'
 __license__ = 'BSD'
 __all__ = ['__title__', '__author__', '__version__', '__license__']
