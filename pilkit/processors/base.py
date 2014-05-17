1cc380f175ab6e972fc2cd956d864214551fbb0d
Petr Vacha <petr@monkeywarrior.com>
2014-05-17 13:24:20 +0200

    Avoid Transpose to crash when exif data are invalid.

--- pilkit/processors/base.py
+++ pilkit/processors/base.py
@@ -161,7 +161,7 @@ class Transpose(object):
             try:
                 orientation = img._getexif()[0x0112]
                 ops = self._EXIF_ORIENTATION_STEPS[orientation]
-            except (KeyError, TypeError, AttributeError):
+            except (IndexError, KeyError, TypeError, AttributeError):
                 ops = []
         else:
             ops = self.methods
