bccc0b915422b0a1f3f0a6bdf1556578855bd69f
Venelin Stoykov <venelin@magicsolutions.bg>
2017-02-17 13:58:01 +0200

    Bump version to 2.0

--- pilkit/pkgmeta.py
+++ pilkit/pkgmeta.py
@@ -1,5 +1,5 @@
 __title__ = 'pilkit'
 __author__ = 'Matthew Tretter'
-__version__ = '1.1.13'
+__version__ = '2.0'
 __license__ = 'BSD'
 __all__ = ['__title__', '__author__', '__version__', '__license__']
