9f55cb23111db210988e789ef69f9aabb0964bbb
Miroslav Kravec <kravec.miroslav@gmail.com>
2019-03-03 09:03:06 +0100

    Test GaussianBlur result (apply review comment)

--- tests/utils.py
+++ tests/utils.py
@@ -2,7 +2,7 @@ import os
 from pilkit.lib import Image
 
 
-def get_image_file():
+def get_image_file(image_name='reference.png'):
     """
     See also:
 
@@ -11,9 +11,21 @@ def get_image_file():
 
     """
     dir = os.path.dirname(__file__)
-    path = os.path.join(dir, 'assets', 'reference.png')
+    path = os.path.join(dir, 'assets', image_name)
     return open(path, 'r+b')
-
 
 def create_image():
     return Image.open(get_image_file())
+
+def compare_images(a, b):
+  if a.size != b.size:
+    return False
+
+  rows, cols = a.size
+
+  for row in range(rows):
+    for col in range(cols):
+      if a.getpixel((row, col)) != b.getpixel((row, col)):
+        return False
+
+  return True
